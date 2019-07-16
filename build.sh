28ec86cefc720647c37ed20bbb31aca564b8dc7d
SSE4 <tomskside@gmail.com>
2019-07-16 20:01:55 +0000

    - fix build script
    Signed-off-by: SSE4 <tomskside@gmail.com>

--- build.sh
+++ build.sh
@@ -7,5 +7,7 @@ mkdir -p build
 conan remote add bc https://api.bintray.com/conan/bincrafters/public-conan || true
 
 conan install . -if build
-cmake -Bbuild -S.
-cmake --build build
+pushd build
+cmake ..
+cmake --build .
+popd
