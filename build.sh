01cb8af130f9bcb5206f3426cc9ca01793d30f63
SSE4 <tomskside@gmail.com>
2019-07-16 20:56:25 +0000

    - build missing
    Signed-off-by: SSE4 <tomskside@gmail.com>

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ mkdir -p build
 
 conan remote add bc https://api.bintray.com/conan/bincrafters/public-conan || true
 
-conan install . -if build
+conan install . -if build --build missing
 pushd build
 cmake ..
 cmake --build .
